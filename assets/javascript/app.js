87ec2dde679c4d978c76bdc9e9f3b8f230a78c6c
Andy Roush <andyroush17@gmail.com>
2018-02-24 10:22:39 -0800

    added header and functionality

---
+++ assets/javascript/app.js
@@ -0,0 +1,94 @@
+// Trivia questions array
+var triviaQuestions = [{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+},{
+    question: "x",
+    answerList: ["x", "x", "x", "x"],
+    answer:x
+}];
+
+var imgArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13', 'question14', 'question15'];
+var currentQuestion
+var correctAnswer
+var incorrectAnswer
+var unanswered
+var seconds
+var time
+var answered
+var userSelect
+var messages = {
+    correct: "Tubular dude!",
+    incorrect: ""
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
